eb6416fb148ac4c025320571c863fc11f5758788
luco17 <lucodonoghue@gmail.com>
2018-12-04 20:38:32 +0000

    Cleaning

--- dt_analytics.py
+++ dt_analytics.py
@@ -1,16 +1,25 @@
-import pandas as pd
+import pandas as pd, os, numpy as np, matplotlib.pyplot as plt
 
-ri = pd.read_csv("RI-clean.csv.gz", low_memory = False)
+ri_raw = pd.read_csv("RI-clean.csv.gz", low_memory = False)
 
-ri.head()
+ri_raw.shape
 
-ri.shape
+ri = ri_raw.sample(n = 91741)
 
-ri2 = ri.sample(n = 91741)
-
-ri.columns.values
-
-ri2 = ri2[['state', 'stop_date', 'stop_time', 'driver_gender', 'driver_race_raw', 
+ri = ri[['stop_date', 'stop_time', 'driver_gender', 'driver_race',
 'violation_raw', 'violation', 'search_conducted', 'search_type', 'stop_outcome', 'is_arrested', 'stop_duration', 'drugs_related_stop', 'district']]
 
 ri.info()
+
+print(ri.isnull().sum())
+
+ri.dropna(subset = ['driver_gender'], inplace = True)
+
+ri['is_arrested'] = ri.is_arrested.astype('bool')
+
+combined = ri.stop_date.str.cat(ri.stop_time, sep = ' ')
+
+ri['stop_datetime'] = pd.to_datetime(combined)
+
+# Set 'stop_datetime' as the index
+ri.set_index('stop_datetime', inplace = True)
